9795da070b1bce74b3116c23b3a7fb0402b95e55
Avneet Singh <avneetmaankiya@gmail.com>
2024-04-02 02:10:53 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/appliances')
+    fetch('https://east-west-aid-api.vercel.app/appliances')
         .then((response) => response.json())
         .then((data) => {
             setPopularProducts(data);
